190d41b8373ec1eead153fa6ec4adbcdebfe6f33
Meinard Edrei <meinardsantoss@gmail.com>
2025-02-23 17:05:03 +0800

    ✨ Initialized contact page

--- app/Contact/page.js
+++ app/Contact/page.js
@@ -3,9 +3,15 @@
 const page = () => {
   return (
     <div>
-
-    </div>
-  )
+      <section className="flex justify-center items-center">
+        <div className="bg-[var(--secondary-background)] md:w-[70%] w-[90%] h-full rounded-md">
+          <div className="flex justify-center items-center bg-[--primary] p-2 text-sm text-[var(--text-inverted)]">contact~</div>
+          <div className="flex flex-col p-5">
+          </div>
+        </div>
+      </section>
+    </div >
+  );
 }
 
 export default page;
