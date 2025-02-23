615c406f6e18c857aeb9269bbdf54549930b13e6
Meinard Edrei <meinardsantoss@gmail.com>
2025-02-23 17:59:18 +0800

    ➕ Added click outside handling for settings

--- app/components/Header.js
+++ app/components/Header.js
@@ -1,25 +1,39 @@
 'use client'
 import { CiSettings } from "react-icons/ci";
 import { IoIosClose } from "react-icons/io";
-import { useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import Link from "next/link";
 
 const Header = () => {
   const [menuOpen, setMenuOpen] = useState(false);
+  const menuRef = useRef();
+
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      if (menuRef.current && !menuRef.current.contains(event.target)) {
+        setMenuOpen(false);
+      }
+    }
+
+    document.addEventListener('mousedown', handleClickOutside);
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside);
+    }
+  }, []);
 
   return (
     <div>
-      <section className="flex md:justify-center md:p-16">
+      <section className="flex md:justify-center md:p-16 mt-[5em] md:mt-0">
         <div className="md:flex hidden gap-5 text-xs">
           <Link href="/About">ABOUT ME (H)</Link>
           <Link href="/Portfolio">PORTFOLIO (J)</Link>
           <Link href="/Contact">CONTACT (L)</Link>
         </div>
-        <div className="fixed top-10 border-[1px] right-5 rounded-md bg-[var(--background)] md:hidden flex place-self-end">
+        <div className="fixed top-10 border-[1px] right-5 rounded-md bg-[var(--background)] opacity-60 md:hidden flex place-self-end">
           <button onClick={() => setMenuOpen(true)} className={menuOpen ? `hidden` : ``}><CiSettings className="text-3xl" /></button>
         </div>
       </section>
-      <div className={`fixed top-0 right-0 z-10 flex flex-col bg-[var(--background)] h-screen p-16 gap-10 
+      <div ref={menuRef} className={`fixed top-0 right-0 z-10 flex flex-col bg-[var(--background)] h-screen p-16 gap-10 
           transition transform duration-300 ease-in-out ${menuOpen ? "translate-x-0" : "translate-x-full"}
         `}>
         <div>
