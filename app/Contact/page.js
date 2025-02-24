1a7ae90fbd7b31685f88fc00e3ea58a052f6332d
Meinard Edrei <meinardsantoss@gmail.com>
2025-02-24 13:49:45 +0800

    🎨 Adjusted window padding

--- app/Contact/page.js
+++ app/Contact/page.js
@@ -12,7 +12,7 @@ const page = () => {
         <div className="bg-[var(--secondary-background)] border-[1px] border-white/15 md:w-[70%] w-[90%] h-full rounded-md">
           <div className="flex justify-center items-center bg-[--primary] p-2 text-sm text-[var(--text-inverted)]">contact~</div>
           <div className="flex flex-col p-5 md:px-10">
-            <div className="p-10">
+            <div className="md:px-10 py-20">
               <div>
                 <h1 className="md:text-2xl text-lg font-semibold mb-5">
                   -- 📥 Get in touch, let&apos;s talk.
@@ -23,37 +23,31 @@ const page = () => {
               </div>
               <div className="flex flex-col gap-5 mt-16">
                 <h1 className="font-medium">
-                  📲 Try my other social media platforms!
+                  -- 📲 Try my other social media platforms!
                 </h1>
-                <div className="flex gap-2">
+                <div className="flex gap-2 md:text-3xl text-2xl">
                   <Link href='https://www.instagram.com/meinardedreii/'>
                     <FaSquareInstagram
-                      className="text-3xl"
                     />
                   </Link>
                   <Link href='https://x.com/meinardedrei'>
                     <FaSquareXTwitter
-                      className="text-3xl"
                     />
                   </Link>
                   <Link href='https://www.facebook.com/profile.php?id=100069723366579'>
                     <FaSquareFacebook
-                      className="text-3xl"
                     />
                   </Link>
                   <Link href='https://github.com/MeinardEdrei'>
                     <FaSquareGithub
-                      className="text-3xl"
                     />
                   </Link>
                   <Link href='https://www.tiktok.com/@meinardedrei'>
                     <AiFillTikTok
-                      className="text-3xl"
                     />
                   </Link>
                   <Link href='https://www.linkedin.com/in/meinard-edrei-santos-8894532b7/'>
                     <FaLinkedin
-                      className="text-3xl"
                     />
                   </Link>
                 </div>
